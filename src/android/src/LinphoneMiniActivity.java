ee838c14c0062e74bf4fc236e7dcf6eff74a27f2
justinlwc <46217897+justinlwc@users.noreply.github.com>
2021-02-25 11:25:58 +0800

    Update LinphoneMiniActivity.java

--- src/android/src/LinphoneMiniActivity.java
+++ src/android/src/LinphoneMiniActivity.java
@@ -99,7 +99,7 @@ public class LinphoneMiniActivity extends Activity {
 
         String videoDeviceId = Linphone.mLinphoneCore.getVideoDevice();
         Linphone.mLinphoneCore.setVideoDevice(videoDeviceId);
-        Linphone.mLinphoneManager.newOutgoingCall(address, displayName, true);
+        //Linphone.mLinphoneManager.newOutgoingCall(address, displayName, true);
 	if (!address.equals("") && !displayName.equals("")){
 		Linphone.mLinphoneManager.newOutgoingCall(address, displayName, true);
 	}
